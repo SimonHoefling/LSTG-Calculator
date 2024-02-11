271445e0740ddc38ad7dbf35c68568a789963bf3
Simon Hoefling <simon@hoeflia.eu>
2024-02-11 10:47:22 +0100

    Fixed some font colors that where not showing up in production

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,8 +29,8 @@ export default function Home() {
   return (
     <main className="flex justify-center items-center min-h-screen">
       <div className="container mx-4 w-full md:w-1/2 shadow-lg shadow-black rounded-lg bg-black">
-        <h1 className='text-3xl text-center mt-2'>Leistungsgradrechner</h1>
-        <h1 className=" text-lg italic text-center mt-2">(Wartezeiten um 106% in 8 Stunden zu erreichen)</h1>
+        <h1 className='text-3xl text-center mt-2 text-white'>Leistungsgradrechner</h1>
+        <h1 className="text-md italic text-center mt-2 text-white">(Wartezeiten um 106% in 8 Stunden zu erreichen)</h1>
         <div className="mt-4 flex justify-center">
           <div className="flex mx-5">
             <input
@@ -56,28 +56,28 @@ export default function Home() {
             <thead>
               <tr>
                 <th className="border border-white"></th>
-                <th className="border border-white px-2">Wartezeit</th>
+                <th className="border border-white px-2 text-white">Wartezeit</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="border border-white px-2">Technisch (ungepl.)</td>
-                <td className="border border-white font-bold text-right px-2">{remainingTime ? `${remainingTime} min` : null}</td>
+                <td className="border border-white px-2 text-white">Technisch (ungepl.)</td>
+                <td className="border border-white font-bold text-right px-2 text-white">{remainingTime ? `${remainingTime} min` : null}</td>
               </tr>
               <tr>
-                <td className="border border-white px-2">Technisch (geplant)</td>
-                <td className="border border-white font-bold text-right px-2">~</td>
+                <td className="border border-white px-2 text-white">Technisch (geplant)</td>
+                <td className="border border-white font-bold text-right px-2 text-white">~</td>
               </tr>
               <tr>
-                <td className="border border-white px-2">Qualität</td>
-                <td className="border border-white font-bold text-right px-2">~</td>
+                <td className="border border-white px-2 text-white">Qualität</td>
+                <td className="border border-white font-bold text-right px-2 text-white">~</td>
               </tr>
             </tbody>
           </table>
         </div>
 
         {remainingTime !== null && (
-          <div className="text-center my-4">
+          <div className="text-center my-4 text-white">
             {remainingTime > 0 ? (
               <p>Wartezeit um 106% in 8 Stunden zu erreichen: {remainingTime} Minuten</p>
             ) : (
